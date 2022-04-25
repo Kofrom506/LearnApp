88ccef0fcfc206de338a80eefbc03b7c1c6287c8
Evan Susanto <evan.susanto@ui.ac.id>
2022-04-25 13:24:36 +0700

    Initial Commit

---
+++ LearnApp/LearnAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnAppApp.swift
+//  LearnApp
+//
+//  Created by Evan Susanto on 25/04/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
